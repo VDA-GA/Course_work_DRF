48a3a59ac4135d7cf5023ee1dff2e11eebcacb21
Dmitry Vasilenko <vasilenkoda91@gmail.com>
2024-06-03 23:15:37 +0300

    add docker

--- config/settings.py
+++ config/settings.py
@@ -27,9 +27,9 @@ load_dotenv(BASE_DIR / ".env")
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
@@ -86,11 +86,11 @@ WSGI_APPLICATION = "config.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("PSQL_NAME"),
-        "USER": os.getenv("PSQL_USER"),
-        "PASSWORD": os.getenv("PSQL_PASSWORD"),
-        "HOST": os.getenv("PSQL_HOST"),
-        "PORT": os.getenv("PSQL_PORT"),
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": os.getenv("POSTGRES_HOST"),
+        "PORT": os.getenv("POSTGRES_PORT"),
     }
 }
 
